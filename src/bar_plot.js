c4153aa2bb92a0a56395700165d4ddc298415ed8
derderianab <derderianab@gmail.com>
2025-01-24 11:33:55 +0100

    fix bar plot display

--- src/bar_plot.js
+++ src/bar_plot.js
@@ -30,27 +30,27 @@
         }));
 
         // Dimensions and margins
-        const margin = { top: 20, right: 30, bottom: 70, left: 50 };
-        const width = 900 - margin.left - margin.right;
+        const margin = { top: 20, right: 300, bottom: 70, left: 50 };
+        const width = 1800 - margin.left - margin.right;
         const height = 400 - margin.top - margin.bottom;
 
         // Append the SVG element
         const svg = d3.select("#bar-chart")
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
-          .append("g")
+            .append("g")
             .attr("transform", `translate(${margin.left},${margin.top})`);
 
         // Set up scales
         const x0 = d3.scaleBand()
             .domain(categories)
             .range([0, width])
-            .padding(0.2);
+            .padding(0.3);
 
         const x1 = d3.scaleBand()
             .domain(series.map(d => d.name))
             .range([0, x0.bandwidth()])
-            .padding(0.05);
+            .padding(0.03);
 
         const y = d3.scaleLinear()
             .domain([d3.min(series.flatMap(d => d.values)) - 1, d3.max(series.flatMap(d => d.values)) + 1])
@@ -62,8 +62,9 @@
             .attr("class", "x-axis")
             .attr("transform", `translate(0,${height})`)
             .call(d3.axisBottom(x0).tickSize(0))
-          .selectAll("text")
+            .selectAll("text")
             .attr("transform", "rotate(-45)")
+            .style("font-size", "14px")
             .style("text-anchor", "end");
 
         svg.append("g")
@@ -89,21 +90,34 @@
 
         // Add legend
         const legend = svg.append("g")
-            .attr("transform", `translate(${width - 100},20)`);
+            .attr("transform", `translate(${width + 20},20)`);
 
+        // Add legend title
+        legend.append("text")
+            .attr("x", 0) 
+            .attr("y", 0) 
+            .attr("text-anchor", "start")
+            .style("font-size", "20px") 
+            .style("font-weight", "bold") 
+            .text("Legend");
+        
+        // Add Legend rows
         series.forEach((s, i) => {
             const legendRow = legend.append("g")
-                .attr("transform", `translate(0,${i * 20})`);
+                .attr("transform", `translate(0,${i * 30})`);
 
             legendRow.append("rect")
                 .attr("width", 10)
                 .attr("height", 10)
+                .attr("x", 0)
+                .attr("y", 30)
                 .attr("fill", d3.schemeCategory10[i]);
 
             legendRow.append("text")
                 .attr("x", 15)
-                .attr("y", 10)
+                .attr("y", 40)
                 .attr("text-anchor", "start")
+                .style("font-size", "12px")
                 .style("text-transform", "capitalize")
                 .text(s.name.replace("z-score ", ""));
         });
